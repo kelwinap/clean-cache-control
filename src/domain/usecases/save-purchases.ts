4eee1067465ec3124b6a68a6c837f7dc5885a7ac
kelwinap <kelwinarthur8@gmail.com>
2021-08-31 15:54:08 -0300

    add save purchase usecase

---
+++ src/domain/usecases/save-purchases.ts
@@ -0,0 +1,13 @@
+export interface SavePurchases {
+    save: (purchases: Array<SavePurchases.Params>) => Promise<void>
+
+}
+
+namespace SavePurchases {
+    export type Params = {
+        id: string,
+        date: Date,
+        value: number
+    }
+}
+
