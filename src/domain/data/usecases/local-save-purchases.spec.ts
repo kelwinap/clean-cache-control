8b9d81ed053a84721583a24baf5467cce3ce2fda
kelwinap <kelwinarthur8@gmail.com>
2021-08-31 16:12:38 -0300

    ensure localsavepurcheses deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,13 +1,21 @@
 class LocalSavePurchases {
     constructor(private readonly cacheStore: CacheStore) { }
+
+    async save(): Promise<void> {
+        this.cacheStore.delete()
+    }
 }
 
 interface CacheStore {
-
+    delete: () => void
 }
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+
+    delete(): void {
+        this.deleteCallsCount++
+    }
 }
 
 describe('LocalSavePurchases', () => {
@@ -16,5 +24,14 @@ describe('LocalSavePurchases', () => {
         const sut = new LocalSavePurchases(cacheStore)
 
         expect(cacheStore.deleteCallsCount).toBe(0)
+    });
+
+    test('should delete old cache on sut.save', async () => {
+        const cacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+
+        sut.save()
+
+        expect(cacheStore.deleteCallsCount).toBe(1)
     });
 });
