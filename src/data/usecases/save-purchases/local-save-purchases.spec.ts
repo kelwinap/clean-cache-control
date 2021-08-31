24b545cfb382037db9123eed5614c62c06ddb15a
kelwinap <kelwinarthur8@gmail.com>
2021-08-31 16:38:49 -0300

    ensure throw if delete fails

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -4,6 +4,7 @@ import { CacheStore } from '@/data/usecases/protocols/cache'
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+    insertCallsCount = 0
     key: string
 
     delete(key: string): void {
@@ -37,5 +38,13 @@ describe('LocalSavePurchases', () => {
         await sut.save()
         expect(cacheStore.deleteCallsCount).toBe(1)
         expect(cacheStore.key).toBe('purchases')
+    });
+
+    test('should not insert new cache if delete fails', async () => {
+        const { cacheStore, sut } = makeSut()
+        jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => { throw new Error() })
+        const promise = sut.save()
+        expect(cacheStore.insertCallsCount).toBe(0)
+        expect(promise).rejects.toThrow()
     });
 });
