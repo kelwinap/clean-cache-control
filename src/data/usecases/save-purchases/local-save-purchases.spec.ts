35ce015fd98572bab547da5565103e3d2c47422b
kelwinap <kelwinarthur8@gmail.com>
2021-09-01 10:40:32 -0300

    use await on async tests

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -33,7 +33,7 @@ describe('LocalSavePurchases', () => {
         cacheStore.simulateDeleteError()
         const promise = sut.save(mockPurchases())
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete])
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     });
 
     test('should insert new cache if delete succeeds', async () => {
@@ -50,6 +50,6 @@ describe('LocalSavePurchases', () => {
         cacheStore.simulateInsertError()
         const promise = sut.save(mockPurchases())
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     });
 });
