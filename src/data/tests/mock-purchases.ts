c34c35bffeeb1b5082287f0ee8f4cd3903aaa977
kelwinap <kelwinarthur8@gmail.com>
2021-09-01 09:52:35 -0300

    add random parameters to mock

---
+++ src/data/tests/mock-purchases.ts
@@ -0,0 +1,12 @@
+import { SavePurchases } from '@/domain/usecases'
+import faker from 'faker'
+
+export const mockPurchases = (): Array<SavePurchases.Params> => [{
+    id: faker.datatype.uuid(),
+    date: faker.date.recent(),
+    value: faker.datatype.number()
+}, {
+    id: faker.datatype.uuid(),
+    date: faker.date.recent(),
+    value: faker.datatype.number()
+}]
--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,7 +1,7 @@
 import { LocalSavePurchases } from '@/data/usecases'
 import { CacheStore } from '@/data/usecases/protocols/cache'
 import { SavePurchases } from '@/domain/usecases'
-
+import { mockPurchases } from '@/data/tests'
 
 class CacheStoreSpy implements CacheStore {
 
@@ -31,15 +31,6 @@ class CacheStoreSpy implements CacheStore {
     }
 }
 
-const mockPurchases = (): Array<SavePurchases.Params> => [{
-    id: '1',
-    date: new Date(),
-    value: 50
-}, {
-    id: '2',
-    date: new Date(),
-    value: 70
-}]
 
 type SutTypes = {
     sut: LocalSavePurchases
